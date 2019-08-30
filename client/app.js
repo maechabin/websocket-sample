55253d0f41ea9cec4616611e81abdf82ade6044f
maeda <takanori@stroly.jp>
2019-08-30 20:15:41 +0900

    first commit

---
+++ client/app.js
@@ -0,0 +1,24 @@
+let data = '';
+
+const sock = new WebSocket('ws://localhost:5001');
+
+// 接続
+sock.addEventListener('open', e => {
+  console.log('Socket 接続成功');
+});
+
+// サーバーからデータを受け取る
+sock.addEventListener('message', e => {
+  console.log(e.data);
+  const message = document.querySelector('.message');
+  data = data + `${e.data}<br>`;
+  message.innerHTML = data;
+});
+
+document.addEventListener('DOMContentLoaded', e => {
+  // サーバーにデータを送る
+  document.querySelector('button').addEventListener('click', e => {
+    const name = document.querySelector('.input').value;
+    sock.send(name);
+  });
+});
---
+++ server.js
@@ -0,0 +1,18 @@
+var server = require('ws').Server;
+var s = new server({ port: 5001 });
+
+s.on('connection', ws => {
+  ws.on('message', message => {
+    console.log('Received: ' + message);
+
+    s.clients.forEach(client => {
+      client.send(message + ' : ' + new Date());
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('I lost a client');
+  });
+});
+
+console.log('WS Server is running: ws://localhost:5001');
